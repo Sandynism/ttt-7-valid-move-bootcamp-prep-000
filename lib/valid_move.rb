082007de44218f8c73be52242cb474f68f3f06f9
Sandy Yeung <sandynism@gmail.com>
2017-10-19 01:08:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,7 @@
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
 def valid_move?(board, index)
   index.between?(1, 9) && !position_taken?(board, index)
 end
